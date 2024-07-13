835740a5edc2e1eff0a80f2ff59564c46ff661b7
Jenya <113167825+Genya10@users.noreply.github.com>
2024-07-13 14:48:07 +0300

    install zustand and create game.types.ts

---
+++ src/store/game/game.types.ts
@@ -0,0 +1,27 @@
+import type { ICard } from "../../types/card.types"
+
+type TPlayer = 'player' | 'opponent'
+
+interface IGameCard extends ICard {
+    id: number
+    isOnBoard: boolean
+    isCanAttack: boolean
+}
+
+interface IHero {
+    deck:IGameCard[]
+    health:number
+    mana: number
+}
+
+export interface IGameStore {
+    player:IHero
+    opponent:IHero
+    currentTurn: TPlayer
+    isGameOver: boolean
+    startGame:()=> void
+    endTurn:() => void
+    playCard:(card: IGameCard) => void
+    attackCard:(attackerId: number, targetId: number) => void
+    attackHero:(attackerId: number) => void
+}
