4e1c5da9b3d41b00891873a6c67a1aeb54774f01
Jenya <113167825+Genya10@users.noreply.github.com>
2024-07-25 21:35:40 +0300

    create Loader and use useTransition

--- src/pages/home/StartedScreen.tsx
+++ src/pages/home/StartedScreen.tsx
@@ -1,16 +1,25 @@
 import { Button } from "../../components/ui/button/Button"
 import { useGameStore } from "../../store/game/game.store"
-import { Heading } from "../../components/ui/heading/Heading";
+import { Heading } from "../../components/ui/heading/Heading"
+import { useTransition } from "react"
+import { Loader } from "../../components/ui/loader/Loader"
 
 export function StartedScreen() {
+  const [isPending, startTransition] = useTransition();
   const { startGame } = useGameStore();
+
+  const onClick = () => {
+   startTransition(()=>{
+    startGame()
+   })
+  }
 
   return (
     <div className="flex items-center justify-center flex-col h-screen">
       <Heading>Max Stone</Heading>
       <br/>
-      <Button variant="primary" isCircle onClick={startGame}>
-        Start game
+      <Button variant="primary" isCircle onClick={onClick}>
+        {isPending ? <Loader/> : 'Start game'}
       </Button>
     </div>
   );
