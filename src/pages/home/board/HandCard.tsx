d86f9325b59c01228dee1403f506e671d1f811e9
Jenya <113167825+Genya10@users.noreply.github.com>
2024-07-27 22:00:53 +0300

    add styles to cover cards with white+cn to BoardC.

--- src/pages/home/board/HandCard.tsx
+++ src/pages/home/board/HandCard.tsx
@@ -1,54 +1,73 @@
 import { ICard } from "../../../types/card.types"
-import { CSSProperties,useState } from "react"
+import { CSSProperties, useState } from "react"
 import cn from "clsx"
 import { getStyleRotation } from "./hand-card/get-style-rotation"
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion"
 
 interface IProps {
-  card: ICard
-  onClick?: () => void
-  isDisabled?: boolean
-  isHided?: boolean
-  style?: CSSProperties
-  index: number
-  arrayLength: number
+  card: ICard;
+  onClick?: () => void;
+  isDisabled?: boolean;
+  isHided?: boolean;
+  style?: CSSProperties;
+  index: number;
+  arrayLength: number;
 }
 
-export function HandCard({card, onClick, isDisabled, isHided, style, 
-  index, arrayLength}: IProps) {
+export function HandCard({
+  card,
+  onClick,
+  isDisabled,
+  isHided,
+  style,
+  index,
+  arrayLength,
+}: IProps) {
+  const [isHovered, setIsHovered] = useState(false);
 
-    const [isHovered, setIsHovered] = useState(false)
-
-    const {rotate, translateY} = getStyleRotation(index, arrayLength, !isHided)
+  const { rotate, translateY } = getStyleRotation(index, arrayLength, !isHided);
 
   return (
     <motion.button
-      className={cn("h-40 w-28 shadow inline-block -ml-8 rounded-2xl cursor-default", {
-        "opacity-60": isDisabled,
-        "cursor-pointer": !isHided && !isDisabled,
-      })}
+      className={cn(
+        "h-40 w-28 shadow inline-block -ml-8 rounded-2xl cursor-default",
+        {
+          "opacity-60": isDisabled,
+          "cursor-pointer": !isHided && !isDisabled,
+        }
+      )}
       style={style}
       disabled={isDisabled}
       onClick={onClick}
-      onMouseEnter={()=> setIsHovered(true)}
-      onMouseLeave={()=> setIsHovered(false)}
-      initial={{scale:1, zIndex:0, y:0}}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      initial={{ scale: 1, zIndex: 0, y: 0 }}
       animate={
         isHovered && !isHided
-        ? {scale:1.2, zIndex:10, y:-90}
-        :{
-          scale:1,
-          zIndex:0,
-          y: translateY,
-          rotate
-        }
+          ? { scale: 1.2, zIndex: 10, y: -90 }
+          : {
+              scale: 1,
+              zIndex: 0,
+              y: translateY,
+              rotate,
+            }
       }
-      transition={{type:'spring', stiffness:200, damping:30}}
-    >     
-     <img src={isHided ? '/assets/heroes/cover.jpg' : card.imageUrl} 
-          alt={card.name} 
-          draggable="false" />         
+      transition={{ type: "spring", stiffness: 200, damping: 30 }}
+    >
+      {isDisabled && (
+        <div
+          className="absolute top-0 left-0 w-full h-full z-10 rounded-lg
+                      flex items-center justify-center"
+        >
+          <div className="w-4/5 h-4/5 bg-black/60 rounded-lg" />
+        </div>
+      )}
+      <img
+        src={isHided ? "/assets/heroes/cover.jpg" : card.imageUrl}
+        alt={card.name}
+        draggable="false"
+        className="will-change-transform"
+      />
     </motion.button>
   );
 }
-
--- src/pages/home/board/board-card/BoardCard.tsx
+++ src/pages/home/board/board-card/BoardCard.tsx
@@ -1,14 +1,18 @@
-import type { ICard } from "../../../../types/card.types";
-import { motion } from "framer-motion";
+import { motion } from "framer-motion"
+import cn from 'clsx'
+import type { IGameCard } from "../../../../store/game/game.types"
 
 interface IProps {
-  card: ICard;
+  card: IGameCard;
 }
 
 export function BoardCard({ card }: IProps) {
   return (
     <motion.button
-      className="h-56 w-40 shadow rounded-2xl"
+      className={cn("h-56 w-40 shadow rounded-2xl border-3", {
+        'border-4':!card.isCanAttack,
+        'cursor-pointer shadow-black': card.isCanAttack
+      })}
       initial={{ scale: 0.7, rotate: -20, y: -100, x: -30, opacity: 0 }}
       animate={{ scale: 1, rotate: 0, y: 0, x: 0, opacity: 1 }}
       transition={{ type: "spring", stiffness: 150, damping: 30, mass: 2 }}
