5fa92dbd8df8c3239e7fe2bcdb0756f6b05295d6
Jenya <113167825+Genya10@users.noreply.github.com>
2024-08-22 13:12:11 +0300

    create setTimeout to gameStore

--- src/store/game/game.store.ts
+++ src/store/game/game.store.ts
@@ -7,12 +7,28 @@ import { attackHeroAction } from "./actions/attack-hero"
 import { returnCardAction } from "./actions/return-card"
 import { initialGameData } from "./initial-data"
 import { startGameAction } from "./actions/start-game/start-game"
+import { randomOpponentPlay } from "./actions/opponent-game/random-opponent-play"
 
-const useGameStore = create<IGameStore>((set, get) => ({
+const useGameStore = create<IGameStore>(set => ({
   ...initialGameData,
-  isGameStarted:false,
+  isGameStarted: false,
   startGame: () => set(startGameAction()),
-  endTurn: () => set(endTurnAction(get)),
+  endTurn: () => {
+    set(endTurnAction)
+  
+    setTimeout(() => {
+      set(state => {
+        const updateState = randomOpponentPlay(state)
+
+        setTimeout(() => {
+          set(() => endTurnAction(updateState))
+      }, 2500)
+
+      return updateState
+    })
+  }, 3000)
+  },
+
   playCard: (cardId:string) => {
     set(state=> playCardAction(state, cardId))
   },
